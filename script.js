9dcb31189df24c7d5e116a8a39f4decf1ef64476
barko-png <shaharedri444@gmail.com>
2025-03-08 07:05:27 +0200

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector(".connect-wallet").addEventListener("click", function() {
-        alert("Wallet connection coming soon!");
-    });
-});
