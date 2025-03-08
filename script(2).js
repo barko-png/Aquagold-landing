e559e63c30ccf5feab733bbc8762f047eefb188c
barko-png <shaharedri444@gmail.com>
2025-03-08 06:56:18 +0200

    Add files via upload

---
+++ script(2).js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".connect-wallet").addEventListener("click", function() {
+        alert("Wallet connection coming soon!");
+    });
+});
